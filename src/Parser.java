4e0c868207124b3eb1ba9b6b4724d486ab551996
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2022-02-12 12:04:18 +0300

    Delete src directory

--- src/Parser.java
+++
@@ -1,5 +0,0 @@
-import java.util.List;
-
-public interface Parser {
-    List<Sonnet> process(String file);
-}
--- src/com/teachmeskills/homework_16/iParser/Parser.java
+++
@@ -1,9 +0,0 @@
-package com.teachmeskills.homework_16.iParser;
-
-import com.teachmeskills.homework_16.model.Sonnet;
-
-import java.util.List;
-
-public interface Parser {
-    List<Sonnet> process(String file);
-}
